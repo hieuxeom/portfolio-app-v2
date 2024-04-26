b943bbb1cec542b3783dfdfbe356252d2cbfc6dd
Trần Ngọc Hiếu <hieuxeom.media.1@gmail.com>
2024-04-26 14:19:42 +0700

    feat: write a new function to help query a row in the database

--- src/helpers/database.ts
+++ src/helpers/database.ts
@@ -1,19 +1,35 @@
 import { ConnectionOptions } from "mysql2";
-import mysql from "mysql2/promise";
+import mysql, { type RowDataPacket } from "mysql2/promise";
 
 export interface IQueryOptions {
 	sql: string;
 	values?: any[];
 }
 
-export async function query({ sql, values = [] }: IQueryOptions) {
-	const connectOptions: ConnectionOptions = {
-		host: process.env.MYSQL_HOST,
-		port: Number(process.env.MYSQL_PORT) || 3000,
-		database: process.env.MYSQL_DATABASE,
-		user: process.env.MYSQL_USER,
-		password: process.env.MYSQL_PASSWORD,
-	};
+type ProjectEnv = "development" | "production" | "test";
+
+const getConnectionOptions = (env: ProjectEnv): ConnectionOptions => {
+	if (env === "production") {
+		return {
+			host: process.env.MYSQL_HOST,
+			port: Number(process.env.MYSQL_PORT) || 3000,
+			database: process.env.MYSQL_DATABASE,
+			user: process.env.MYSQL_USER,
+			password: process.env.MYSQL_PASSWORD,
+		};
+	} else {
+		return {
+			host: process.env.MYSQL_HOST_DEV,
+			port: Number(process.env.MYSQL_PORT_DEV) || 3000,
+			database: process.env.MYSQL_DATABASE_DEV,
+			user: process.env.MYSQL_USER_DEV,
+			password: process.env.MYSQL_PASSWORD_DEV,
+		};
+	}
+};
+
+export async function query({ sql, values = [] }: IQueryOptions): Promise<RowDataPacket[]> {
+	const connectOptions: ConnectionOptions = getConnectionOptions(process.env.NODE_ENV);
 
 	const connectDb = await mysql.createConnection(connectOptions);
 
@@ -21,9 +37,24 @@ export async function query({ sql, values = [] }: IQueryOptions) {
 		const [results] = await connectDb.execute(sql, values);
 
 		connectDb.end();
-		return results;
+		return results as RowDataPacket[];
 	} catch (error) {
 		console.log(error);
-		return null;
+		return [];
+	}
+}
+export async function queryOne({ sql, values = [] }: IQueryOptions): Promise<RowDataPacket> {
+	const connectOptions: ConnectionOptions = getConnectionOptions(process.env.NODE_ENV);
+
+	const connectDb = await mysql.createConnection(connectOptions);
+
+	try {
+		const [results] = await connectDb.execute(sql, values);
+
+		connectDb.end();
+		return (results as RowDataPacket[])[0];
+	} catch (error) {
+		console.log(error);
+		return {} as RowDataPacket;
 	}
 }
